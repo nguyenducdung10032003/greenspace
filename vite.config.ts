4a3b0da24b5794200374b3dcedd71623610e98b6
NguyenDucDungHE171774 <dungndhe171774@fpt.edu.vn>
2025-10-15 16:14:30 +0700

    ok123

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   build: {
     outDir: "dist",
-  },
-  server: {
-    port: 3000,
-    open: true,
   },
 });
